2c588cf56bf135d2d9843273dd9a20d94283adcb
Justin Menga <justin.menga@gmail.com>
2020-03-29 14:18:18 +1300

    Creating the sample application

---
+++ src/todobackend/settings/test.py
@@ -0,0 +1,37 @@
+from .base import *
+import os
+
+# Installed Apps
+INSTALLED_APPS += ('django_nose', )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+NOSE_ARGS = [
+    '--verbosity=2',                                        # verbose output
+    '--nologcapture',                                       # don't output log capture
+    '--with-coverage',                                      # activate coverage report
+    '--cover-package=todo',                                 # coverage reports will apply to these packages
+    '--with-spec',                                          # spec style tests
+    '--spec-color',                                         # make them pretty
+    '--with-xunit',                                         # enable xunit plugin
+    f'--xunit-file={TEST_OUTPUT_DIR}/nosetests.xml',        # xunit test output
+    '--cover-xml',                                          # produce XML coverage info
+    f'--cover-xml-file={TEST_OUTPUT_DIR}/coverage.xml',     # XML coverage output
+]
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+DATABASES = {
+    'default': {
+        # Supports AWS X-Ray
+        'ENGINE': 'mysql.connector.django',
+        'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
+        'USER': os.environ.get('MYSQL_USER','todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+        'HOST': os.environ.get('MYSQL_HOST','localhost'),
+        'PORT': os.environ.get('MYSQL_PORT','3306'),
+        # See https://code.djangoproject.com/ticket/30469#comment:5
+        'OPTIONS': {
+            'use_pure': True
+        }
+    }
+}
