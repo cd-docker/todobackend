222f6faccb748a4c598c0a4bf7b11c76ae26b9ca
Justin Menga <justin.menga@gmail.com>
2020-04-12 13:11:46 +1200

    Add acceptance tests

---
+++ tests/acceptance/conftest.py
@@ -0,0 +1,46 @@
+import os
+
+import requests
+import pytest
+
+@pytest.fixture(scope='session')
+def url():
+    """Test Server URL Fixture"""
+    yield os.environ.get('TEST_URL', 'http://localhost:8000/todos')
+
+
+@pytest.fixture
+def session():
+    """HTTP client session fixture"""
+    session = requests.session()
+    session.headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    yield session
+
+
+@pytest.fixture
+def origin(session):
+    """HTTP client session fixture with origin header"""
+    session.headers = {
+        **session.headers,
+        'Origin': 'http://example.com'
+    }
+    yield session
+
+
+@pytest.fixture
+def item():
+    """Todo item"""
+    yield { 
+        'title': 'Walk the dog',
+        'completed': False,
+        'order': 1
+    }
+
+
+@pytest.fixture
+def create_item(session, url, item):
+    """Creates todo item"""
+    yield session.post(url, json=item)
